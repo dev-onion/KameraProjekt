5b8d912e6acf3672827891058ec6719821513d39
dev-onion <dev-onion@protonmail.com>
2021-12-22 16:23:40 +0100

    *testing* ersten view erstellt (systemüberwachung)

--- KameraProjekt/website/views.py
+++ KameraProjekt/website/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def systemueberwachung(request):
+    ort = "systemüberwachung"
+    return render(request, 'website/systemüberwachung.html', {'ort' : ort})
